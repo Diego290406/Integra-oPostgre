a2724a3b2a7a4cacdc7780e024995eaf3d33972a
Diego Cunha da Silva <cunhadiego529@gmail.com>
2025-09-04 21:08:54 -0300

    commits finais

--- src/main/java/com/seuusuario/app/dao/ConnectionFactory.java
+++ src/main/java/com/seuusuario/app/dao/ConnectionFactory.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private static final String URL      = "jdbc:postgresql://localhost:5432/seu_banco";
-    private static final String USUARIO  = "seu_usuario";
-    private static final String SENHA    = "sua_senha";
+    private static final String URL      = "jdbc:postgresql://localhost:5432/teste";
+    private static final String USUARIO  = "meu_usuario";
+    private static final String SENHA    = "minhaSenha";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
--- src/main/java/com/seuusuario/app/dao/UsuarioDAO.java
+++ src/main/java/com/seuusuario/app/dao/UsuarioDAO.java
@@ -26,18 +26,30 @@ public class UsuarioDAO {
             return resultados;
         }
     }
+    public int getNextCodigo() throws SQLException {
+    	  String seq = "SELECT COALESCE(MAX(codigo), 0) + 1 FROM public.usuarios";
+    	  try (Connection c = ConnectionFactory.getConnection();
+    	       Statement s = c.createStatement();
+    	       ResultSet rs = s.executeQuery(seq)) {
+    	    rs.next();
+    	    return rs.getInt(1);
+    	  }
+    	}
+
 
     public void inserir(Usuario u) throws SQLException {
-        String sql = "INSERT INTO public.usuarios(login, senha, sexo) VALUES (?, ?, ?)";
-        try (Connection conn = ConnectionFactory.getConnection();
-             PreparedStatement ps = conn.prepareStatement(sql)) {
+    	  int novoCodigo = getNextCodigo();
+    	  String sql = "INSERT INTO public.usuarios(codigo, login, senha, sexo) VALUES(?, ?, ?, ?)";
+    	  try (Connection c = ConnectionFactory.getConnection();
+    	       PreparedStatement ps = c.prepareStatement(sql)) {
+    	    ps.setInt(1, novoCodigo);
+    	    ps.setString(2, u.getLogin());
+    	    ps.setString(3, u.getSenha());
+    	    ps.setString(4, String.valueOf(u.getSexo()));
+    	    ps.executeUpdate();
+    	  }
+    	}
 
-            ps.setString(1, u.getLogin());
-            ps.setString(2, u.getSenha());
-            ps.setString(3, String.valueOf(u.getSexo()));
-            ps.executeUpdate();
-        }
-    }
 
     public void excluir(int codigo) throws SQLException {
         String sql = "DELETE FROM public.usuarios WHERE codigo = ?";
